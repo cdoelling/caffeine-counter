2b0ef4a866c7a97221c8d625b2cf10bec9504b53
Chase <whinston.irving@gmail.com>
2015-06-04 16:44:39 -0600

    Finish images

---
+++ counter.js
@@ -0,0 +1,30 @@
+
+listen click numbers{
+  reveal types
+}
+
+listen click type {
+  reveal time
+}
+
+listen click time {
+  reveal button
+}
+
+count == value
+
+var
+
+
+var counter () {
+
+active quantity * active kind = caffience in system
+
+caffience * now = repeat (caffiene/2) until <5mg
+
+number of times divided * 5
+
+display 
+
+
+}
